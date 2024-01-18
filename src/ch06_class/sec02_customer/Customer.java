d276bf540ca6e6dbd52342f85a79a68bd4fe7441
Cheolkie Kim <ckiekim@naver.com>
2024-01-18 11:58:50 +0900

    6장. 클래스

---
+++ src/ch06_class/sec02_customer/Customer.java
@@ -0,0 +1,11 @@
+package ch06_class.sec02_customer;
+
+public class Customer {
+	private int cid;
+	private String name;
+	private int age;
+	private boolean adult;
+	// 생성자 (Constructor)
+	// toString() method
+	// Getter/Setter method
+}
