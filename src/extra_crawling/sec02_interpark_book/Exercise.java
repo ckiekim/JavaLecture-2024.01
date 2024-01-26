1bd7ab98598ffd740799f316bb88f453d522e6fa
Cheolkie Kim <ckiekim@naver.com>
2024-01-26 15:50:41 +0900

    크롤링

--- src/extra_crawling/sec02_interpark_book/Exercise.java
+++ src/extra_crawling/sec02_interpark_book/Exercise.java
@@ -16,13 +16,22 @@ public class Exercise {
 		Elements lis = doc.select(".rankBestContentList > ol > li");
 		System.out.println(lis.size());
 		
-		Element li = lis.get(10);
+		Element li = lis.get(14);
 		String title = li.selectFirst(".itemName").text().strip();
 		String author = li.selectFirst(".author").text().strip();
 		String company = li.selectFirst(".company").text().strip();
 		String price_ = li.selectFirst(".price > em").text().strip();
 		int price = Integer.parseInt(price_.replace(",", ""));
 		System.out.println(title + ", " + author + ", " + company + ", " + price);
+		
+		Elements spans = li.select(".rankNumber.digit2 > span");
+		String rank_ = "";
+		for (Element span: spans) {
+			String s = span.attr("class").strip();
+			rank_ += s.charAt(s.length() - 1);
+		}
+		int rank = Integer.parseInt(rank_);
+		System.out.println(rank);
 	}
 
 }
---
+++ src/extra_crawling/sec03_melon/MelonChart.java
@@ -0,0 +1,9 @@
+package extra_crawling.sec03_melon;
+
+public class MelonChart {
+	private int rank;
+	private String title;
+	private String artist;
+	private String album;
+	private String imgSrc;
+}
