8e4a0e36d5d20b5ffcf89376af80b0a7813779bf
Cheolkie Kim <ckiekim@naver.com>
2024-01-31 10:09:35 +0900

    REST API

--- src/extra_api/KakaoLocalApi.java
+++ src/extra_api/KakaoLocalApi.java
@@ -7,7 +7,12 @@ import java.net.HttpURLConnection;
 import java.net.URI;
 import java.net.URL;
 import java.net.URLEncoder;
+import java.util.HashMap;
 import java.util.Map;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
 
 /**
  * 도로명주소로부터 위도(latitude), 경도(longitude) 정보를 구해주는 메소드
@@ -32,7 +37,7 @@ public class KakaoLocalApi {
 		
 		// 응답 결과 확인
 		int responseCode = conn.getResponseCode();
-		System.out.println(responseCode);
+//		System.out.println(responseCode);
 		
 		br = new BufferedReader(new InputStreamReader(conn.getInputStream(), "utf-8"));
 		StringBuffer sb = new StringBuffer();
@@ -40,8 +45,21 @@ public class KakaoLocalApi {
 		while((line = br.readLine()) != null)
 			sb.append(line);
 		br.close();
-		System.out.println(sb.toString());
+//		System.out.println(sb.toString());
 		
-		return null;
+		// JSON 데이터에서 원하는 값 추출하기
+    	JSONParser parser = new JSONParser();
+    	JSONObject object = (JSONObject) parser.parse(sb.toString());
+    	JSONArray documents = (JSONArray) object.get("documents");
+    	JSONObject item = (JSONObject) documents.get(0);
+//    	System.out.println(item.keySet());	// [address, address_type, x, y, address_name, road_address]
+    	String lon_ = (String) item.get("x");
+    	String lat_ = (String) item.get("y");
+//    	System.out.println(lon_ + ", " + lat_);
+    	
+    	Map<String, Double> map = new HashMap<String, Double>();
+    	map.put("lon", Double.parseDouble(lon_));
+    	map.put("lat", Double.parseDouble(lat_));
+		return map;
 	}
 }
