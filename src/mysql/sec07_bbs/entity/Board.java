ac7a2ea27e448fd2804220e390e093a5ade53671
Cheolkie Kim <ckiekim@naver.com>
2024-02-07 16:07:16 +0900

    MySQL - bbs

---
+++ src/mysql/sec07_bbs/entity/Board.java
@@ -0,0 +1,14 @@
+package mysql.sec07_bbs.entity;
+
+import java.time.LocalDateTime;
+
+public class Board {
+	private int bid;
+	private String title;
+	private String content;
+	private String uid;
+	private LocalDateTime modTime;
+	private int isDeleted;
+	private int viewCount;
+	private int replyCount;
+}
