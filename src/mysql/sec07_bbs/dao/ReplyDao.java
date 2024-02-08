46b025b482e63c71d23c1f1cd6c7ffa3179d8dca
Cheolkie Kim <ckiekim@naver.com>
2024-02-08 14:21:47 +0900

    MySQL - bbs

--- src/mysql/sec07_bbs/dao/ReplyDao.java
+++ src/mysql/sec07_bbs/dao/ReplyDao.java
@@ -3,6 +3,10 @@ package mysql.sec07_bbs.dao;
 import java.io.FileInputStream;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Properties;
 
@@ -46,12 +50,42 @@ public class ReplyDao {
 	}
 	
 	public List<Reply> getReplyList(int bid) {
-		
-		return null;
+		String sql = "SELECT r.*, u.uname FROM reply r"
+					+ "	JOIN users u ON r.uid=u.uid"
+					+ "	WHERE r.bid=?"
+					+ "	ORDER BY rid";
+		List<Reply> list = new ArrayList<Reply>();
+		try {
+			PreparedStatement pstmt = conn.prepareStatement(sql);
+			pstmt.setInt(1, bid);
+			
+			ResultSet rs = pstmt.executeQuery();
+			while (rs.next()) {
+				Reply r = new Reply(rs.getInt(1), rs.getString(2),
+							LocalDateTime.parse(rs.getString(3).replace(" ", "T")),
+							rs.getString(4), rs.getInt(5), rs.getString(6));
+				list.add(r);
+			}
+			rs.close(); pstmt.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return list;
 	}
 	
 	public void insertReply(Reply reply) {
-		
+		String sql = "insert into reply values (default, ?, default, ?, ?)";
+		try {
+			PreparedStatement pstmt = conn.prepareStatement(sql);
+			pstmt.setString(1, reply.getComment());
+			pstmt.setString(2, reply.getUid());
+			pstmt.setInt(3, reply.getBid());
+			
+			pstmt.executeUpdate();
+			pstmt.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 
 }
