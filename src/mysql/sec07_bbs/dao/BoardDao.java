5bc3e928e91ae68172e8c30dbf76f38a819a49f3
Cheolkie Kim <ckiekim@naver.com>
2024-02-08 11:45:01 +0900

    MySQL - bbs

--- src/mysql/sec07_bbs/dao/BoardDao.java
+++ src/mysql/sec07_bbs/dao/BoardDao.java
@@ -6,6 +6,7 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Properties;
 
@@ -68,12 +69,45 @@ public class BoardDao {
 	// field 값은 title, content, uid 등 attribute name
 	// query 값은 검색어
 	public List<Board> getBoardList(String field, String query, int num, int offset) {
-		
-		return null;
+		String sql = "SELECT b.*, u.uname FROM board b"
+					+ "	JOIN users u ON b.uid=u.uid"
+					+ "	WHERE b.isDeleted=0 AND " + field + " LIKE ?"
+					+ "	ORDER BY bid DESC "
+					+ "	LIMIT ? OFFSET ?";
+		List<Board> list = new ArrayList<Board>();
+		try {
+			PreparedStatement pstmt = conn.prepareStatement(sql);
+			pstmt.setString(1, query);
+			pstmt.setInt(2, num);
+			pstmt.setInt(3, offset);
+			
+			ResultSet rs = pstmt.executeQuery();
+			while (rs.next()) {
+				Board board = new Board(rs.getInt(1), rs.getString(2), rs.getString(3), rs.getString(4), 
+						LocalDateTime.parse(rs.getString(5).replace(" ", "T")),
+						rs.getInt(6), rs.getInt(7), rs.getInt(8), rs.getString(9));
+				list.add(board);
+			}
+			rs.close(); pstmt.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return list;
 	}
 	
 	public void insertBoard(Board board) {
-		
+		String sql = "insert into board values (default, ?, ?, ?, default, default, default, default)";
+		try {
+			PreparedStatement pstmt = conn.prepareStatement(sql);
+			pstmt.setString(1, board.getTitle());
+			pstmt.setString(2, board.getContent());
+			pstmt.setString(3, board.getUid());
+			
+			pstmt.executeUpdate();
+			pstmt.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 	
 	public void updateBoard(Board board) {
--- src/mysql/sec07_bbs/dao/BoardDaoTest.java
+++ src/mysql/sec07_bbs/dao/BoardDaoTest.java
@@ -1,5 +1,7 @@
 package mysql.sec07_bbs.dao;
 
+import java.util.List;
+
 import mysql.sec07_bbs.entity.Board;
 
 public class BoardDaoTest {
@@ -9,6 +11,22 @@ public class BoardDaoTest {
 
 		Board b = bDao.getBoard(1);
 		System.out.println(b);
+		
+		b = new Board("제목 12", "본문 12", "james");
+		bDao.insertBoard(b);
+		b = new Board("제목 13", "본문 13", "james");
+		bDao.insertBoard(b);
+		b = new Board("제목 14", "본문 14", "maria");
+		bDao.insertBoard(b);
+		b = new Board("제목 15", "본문 15", "sarah");
+		bDao.insertBoard(b);
+		b = new Board("제목 16", "본문 16", "brian");
+		bDao.insertBoard(b);
+		
+		List<Board> list = bDao.getBoardList("title", "%", 10, 0);
+		list.forEach(x -> System.out.println(x));
+		
+		bDao.close();
 	}
 
 }
